4d6377001f724b993ff1860b5f59e8eef35f462b
David Zoltok <dzoltok@thestar.ca>
2015-08-04 11:23:20 -0400

    Added a few comments for clarity

--- app/controllers/analysis_controller.rb
+++ app/controllers/analysis_controller.rb
@@ -12,22 +12,28 @@ class AnalysisController < ApplicationController
     max_id = nil
     tweets_processed = 0
     hashtags = Hash.new(0)
+
+    # Repeat this loop until no more tweets are received OR <limit> tweets have been processed OR all remaining tweets are already in the DB
     catch (:finished) do
       loop do
+        # Grab a single page of up to 200 tweets with ID less than the maximum ID already read
         if max_id
           tweets = twitter.user_timeline(handle, count: 200, max_id: (max_id - 1), trim_user: true, exclude_replies: false, include_rts: true)
         else
           tweets = twitter.user_timeline(handle, count: 200, trim_user: true, exclude_replies: false, include_rts: true)
         end
+        # If no tweets were found, the algorithm is done
         if tweets.empty?
           throw :finished
         end
+        # For each tweet, we store its ID as the new max ID, and scan its hashtags, incrementing every found hashtag count by 1
         tweets.each do |tweet|
           max_id = tweet.id
           tweet.hashtags.each do |hashtag|
             hashtags[hashtag.text] += 1
           end
           tweets_processed += 1
+          # If we've already processed the maximum number of tweets, the algorithm is done
           if tweets_processed >= limit.to_i
             throw :finished
           end
@@ -35,6 +41,7 @@ class AnalysisController < ApplicationController
       end
     end
 
+    # Sort the resulting hashtags by descending count value, grab the first 10 elements, and return them as a new Hash
     render json: hashtags.sort_by { |h, v| -v }[0..9].to_h
   end
 end
